9c63477a5ff4c345ad2d053e9afb76f7b0480785
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-02-18 23:24:36 -0400

    se crea archivo 'strings.js'

---
+++ strings.js
@@ -0,0 +1,16 @@
+let x = 'dog'
+let m = 'montevideo'
+console.log(x.charAt(1)); // devuelve el segundo index
+console.log(x.charAt(0)); // devuelve el primer index
+
+console.log(m.substring(1,4));
+console.log(m.substring(3,7));
+console.log(m.substring(3)); // retorna los valores desde el indice al final del string
+
+// string comparison
+console.log(x > m);
+console.log(x < m);
+console.log(x == m);
+console.log('a' > 'b');
+
+// string search
