cb74610566b9413b155a7038bffa78f8f9994fab
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-02-19 13:39:44 -0400

    Se agregan loc

--- strings.js
+++ strings.js
@@ -1,16 +1,33 @@
 let x = 'dog'
 let m = 'montevideo'
-console.log(x.charAt(1)); // devuelve el segundo index
-console.log(x.charAt(0)); // devuelve el primer index
+console.log(x.charAt(1)) // devuelve el segundo index
+console.log(x.charAt(0)) // devuelve el primer index
 
-console.log(m.substring(1,4));
-console.log(m.substring(3,7));
-console.log(m.substring(3)); // retorna los valores desde el indice al final del string
+console.log(m.substring(1, 4))
+console.log(m.substring(3, 7))
+console.log(m.substring(3)) // retorna los valores desde el indice al final del string
 
 // string comparison
-console.log(x > m);
-console.log(x < m);
-console.log(x == m);
-console.log('a' > 'b');
+console.log(x > m)
+console.log(x < m)
+console.log(x == m)
+console.log('a' > 'b')
 
 // string search
+let e = 'Enlightement'
+console.log(e.startsWith('En')); // returns true
+console.log(e.endsWith('nt')); // returns true
+
+// STRING DECOMPOSITION
+var test1 = 'chicken,noodle,soup,broth';
+console.log(test1); // nos devuelve un string
+console.log(test1.split(',')); // nos devuelve un array con cada palabra en un index diferente
+console.log(test1.split(',')[1]); // imprimimos el index '1' (noodle)
+
+console.log(e.split('')); // nos devuelve cada letra por separado en un array
+
+// STRING REPLACE
+let wiz = "Wizard of Oz"
+console.log(wiz);
+console.log(wiz.replace('Wizard', 'Witch')); // nos devuelve el string "Witch of Oz"
+
