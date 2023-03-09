b31a06c7536c098e331db3e897fa480d05c79bc1
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-08 22:47:16 -0400

    se agrega modulo 'short circuit'

---
+++ short-circuit.js
@@ -0,0 +1,7 @@
+const saludo = "hola";
+
+// se ejecuta si la condicion es verdadera
+saludo == "hola" && console.log("Esto se Ejecuta");
+
+// se ejecuta si la condicion es falsa
+saludo == 23 || console.log("Esto se ejecuta");
