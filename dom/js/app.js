68fea362679992e63128032d619cbb88ad63affd
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-18 13:35:37 -0400

    se agrega proyecto 'DOM Manipulation'

---
+++ dom/js/app.js
@@ -0,0 +1,6 @@
+let element = document.getElementById("card")
+element.addEventListener("click", () => alert("Hola"))
+
+let cardDos = document.querySelector(".card-dos")
+cardDos.addEventListener("click", () => cardDos.style.backgroundColor = "yellow")
+
