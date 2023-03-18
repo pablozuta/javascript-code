5557718b64613605629d67527cd47d7446e69212
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-18 19:28:48 -0400

    se agrega proyecto 'jest test'

--- dom/js/app.js
+++ dom/js/app.js
@@ -1,6 +1,10 @@
-let element = document.getElementById("card")
-element.addEventListener("click", () => alert("Hola"))
+let element = document.getElementById('card');
+element.addEventListener('click', () => alert('Hola'));
 
-let cardDos = document.querySelector(".card-dos")
-cardDos.addEventListener("click", () => cardDos.style.backgroundColor = "yellow")
+let cardDos = document.querySelector('.card-dos');
+cardDos.addEventListener(
+  'click',
+  () => (cardDos.style.backgroundColor = 'yellow')
+);
+
 
---
+++ test-jest/README.md
@@ -0,0 +1,12 @@
+package.json
+
+ ```"scripts": {
+    "test": "jest"
+  }
+```
+
+```
+npm test
+```
+
+nombre archivo + test + js
